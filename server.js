cb05f7fbb27b29e31ba76e83bcfd1ff237b2c7c6
ayo83 <oayomikun83@gmail.com>
2021-01-26 01:41:22 +0100

    Fixing Bug

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
+
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 
-// Initialize Server
 const app = express();
 
-// Connect DB
+// Connect Database
 connectDB();
 
-// Initialize Middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
-
-// Route Testing
-// app.get('/', (req, res) => { res.json({ msg: 'Welcome to the Contact Keeper API.......'})});
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes//auth'));
-app.use('/api/contact', require('./routes/contact'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production') {
-    // Set Static folder
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (re, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));)
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
 
-// Creating Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
