31bc9394e09170df555256b013f3dc3ffc8d3987
ayo83 <oayomikun83@gmail.com>
2021-01-26 01:32:21 +0100

    Preping for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Initialize Server
 const app = express();
@@ -11,12 +12,20 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Route Testing
-app.get('/', (req, res) => { res.json({ msg: 'Welcome to the Contact Keeper API.......'})});
+// app.get('/', (req, res) => { res.json({ msg: 'Welcome to the Contact Keeper API.......'})});
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes//auth'));
 app.use('/api/contact', require('./routes/contact'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set Static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (re, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));)
+}
 
 // Creating Port
 const PORT = process.env.PORT || 5000;
